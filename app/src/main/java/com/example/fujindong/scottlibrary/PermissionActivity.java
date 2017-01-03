a76925c95a34c63ec144c750322db56a3bb89703
fujindong <alanfujindong@gmail.com>
2017-01-03 14:31:17 +0800

    测试

--- app/src/main/java/com/example/fujindong/scottlibrary/PermissionActivity.java
+++ app/src/main/java/com/example/fujindong/scottlibrary/PermissionActivity.java
@@ -14,6 +14,7 @@ public class PermissionActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission);
+//        添加activity
         ActivityCollector.addActivity(this);
     }
 
